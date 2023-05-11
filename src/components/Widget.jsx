6136bbcdb3a8877b450906cd71b9141c8ffd36e9
Kevin Le Faucheur <lefaucheur.ke@gmail.com>
2023-05-12 01:39:30 +0200

    Widget collapse

--- src/components/Widget.jsx
+++ src/components/Widget.jsx
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
 const Button = styled.div`
   display: flex;
+  flex-direction: column;
   justify-content: space-between;
   align-items: center;
   min-width: 7rem;
-  height: 30px;
-  background-color: #73aabd;
+  background-color: #eee;
   border-radius: 5px;
-  padding: 20px;
+  padding: 10px;
   margin-right: 5px;
+
+  .widget__header {
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+  }
 
   p {
     font-size: 1rem;
@@ -29,12 +35,34 @@ const Button = styled.div`
   }
 `
 
+const Option = styled.div`
+  display: flex;
+  justify-content: space-between;
+`
+
 export const Widget = ({ prompt }) => {
+  const [isOpen, setIsOpen] = useState();
+
+  const handleIsOpen = () => {
+    setIsOpen(!isOpen);
+  }
+
+  const handleColor = (value) => {
+    // console.log(value);
+  }
+
   return (
-    <Button>
-      <i className="fa-solid fa-angle-up" />
-      <p>{prompt}</p>
-      <button className="fa-solid fa-xmark" />
+    <Button >
+      <div className='widget__header' onClick={handleIsOpen}>
+        <i className="fa-solid fa-angle-up" />
+        <p>{prompt}</p>
+        <button className="fa-solid fa-xmark" />
+      </div>
+      {isOpen ?
+      <div>
+        <Option><p>Weight:</p><input type="range" min={0} max={10} step={0.1} /></Option>
+        <Option><p>Color:</p><input type="color" onChange={(event) => handleColor(event.target.value)} /></Option>
+      </div> : ''}
     </Button>
   )
 }
