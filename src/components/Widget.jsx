32cd34764d81e55dd0ed1276c149a7ba9368dede
Kevin Le Faucheur <lefaucheur.ke@gmail.com>
2023-05-17 11:47:20 +0200

    onBlur change widget color

--- src/components/Widget.jsx
+++ src/components/Widget.jsx
@@ -6,8 +6,7 @@ const Button = styled.div`
   flex-direction: column;
   justify-content: space-between;
   align-items: center;
-  min-width: 7rem;
-  background-color: #eee;
+  background-color: ${props => props.backgroundColor};
   border-radius: 5px;
   padding: 10px;
   margin-right: 5px;
@@ -16,12 +15,18 @@ const Button = styled.div`
     display: flex;
     justify-content: space-between;
     width: 100%;
+    cursor: pointer;
+  }
+
+  i {
+    padding: 0 0.5rem;
   }
 
   p {
     font-size: 1rem;
     line-height: 1rem;
     margin: 0;
+    padding: 0 0.5rem;
   }
 
   button {
@@ -42,26 +47,29 @@ const Option = styled.div`
 
 export const Widget = ({ prompt }) => {
   const [isOpen, setIsOpen] = useState();
+  const [categoryColor, setCategoryColor] = useState('#eee');
+
 
   const handleIsOpen = () => {
     setIsOpen(!isOpen);
+    // setCategoryColor(value);
   }
 
   const handleColor = (value) => {
-    // console.log(value);
+    setCategoryColor(value);
   }
 
   return (
-    <Button >
+    <Button backgroundColor={categoryColor}>
       <div className='widget__header' onClick={handleIsOpen}>
         <i className="fa-solid fa-angle-up" />
         <p>{prompt}</p>
         <button className="fa-solid fa-xmark" />
       </div>
       {isOpen ?
-      <div>
+      <div className='widget__body'>
         <Option><p>Weight:</p><input type="range" min={0} max={10} step={0.1} /></Option>
-        <Option><p>Color:</p><input type="color" onChange={(event) => handleColor(event.target.value)} /></Option>
+        <Option><p>Color:</p><input type="color" onBlur={(event) => handleColor(event.target.value)} /></Option>
       </div> : ''}
     </Button>
   )
