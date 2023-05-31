94d8085803be89ddd379fe7becda8f2dd1a7f00b
Kevin Le Faucheur <lefaucheur.ke@gmail.com>
2023-05-31 17:26:34 +0200

    Calculating parenthesis and brackets weights basic

--- src/utils.js
+++ src/utils.js
@@ -4,11 +4,20 @@ export const promptParser = (prompt) => {
 
   keywords.forEach(keyword => {
     let newKeyword = keyword.trim();
+    let weight = 1;
 
+    // console.log(newKeyword.match(/\([^)]*\)|\[[^\]]*\]/g));
+    let parenthesisCount = (newKeyword.match(/\(/g) || []).length; // match returns null with no result, || []
+    for (let i = 1; i <= parenthesisCount; i++) {
+      weight *= 1.1;
+    }
+    let bracketsCount = (newKeyword.match(/\[/g) || []).length;
+    for (let i = 1; i <= bracketsCount; i++) {
+      weight /= 1.1;
+    }
 
     // Checks for a weight
     const weightedKeyword = keyword.split(':');
-    let weight = 1;
 
     if(weightedKeyword.length > 1 && weightedKeyword[1] !== '') {
       newKeyword = weightedKeyword[0];
@@ -22,6 +31,8 @@ export const promptParser = (prompt) => {
       console.log(weight);
       console.log(weightedKeyword[1].match(/(\d+)(.\d+)?/g));
     }
+
+    weight = weight.toFixed(2);
 
     // Push if not empty string
     if(newKeyword !== '') newPrompt.push({ prompt: newKeyword, weight: weight, color: '#9EE' });
