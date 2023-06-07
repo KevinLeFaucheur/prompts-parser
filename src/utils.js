69aad16602bdb1a6835151aad23e67d04bfa4295
Kevin Le Faucheur <lefaucheur.ke@gmail.com>
2023-06-07 20:05:29 +0200

    Tests for weight property

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,20 @@
-export const promptParser = (prompt) => {
-  return [{ prompt: prompt }];
+export const promptParser = (prompts) => {
+  let keywords = prompts.split(',');
+  let prompt = 'Error';
+  let weight = 1;
+
+  return keywords.map(keyword => { 
+    let weightedWord = keyword.split(':');
+
+    prompt = weightedWord[0].trim();
+    weight = weightedWord[1] ? parseFloat(weightedWord[1]) : 1;
+
+    return { prompt: prompt, weight: weight } 
+  });
+}
+
+export const validatePrompt = (prompt) => {
+  return !(prompt === '');
 }
 
 export const colors = [
--- src/utils.test.js
+++ src/utils.test.js
@@ -2,7 +2,10 @@
 
 import { promptParser } from "./utils";
 
-it('should return an array of objects', () => { 
-  expect(promptParser('hair')).toStrictEqual ([{ prompt:'hair' }]);
+it('should return an array of prompt objects', () => { 
+  expect(promptParser('hair')).toStrictEqual([{ prompt:'hair', weight: 1 }]);
+  expect(promptParser('hair, eyes')).toStrictEqual([{ prompt: 'hair', weight: 1 }, { prompt: 'eyes', weight: 1}]);
+  expect(promptParser('brown hair:0.5')).toStrictEqual([{ prompt:'brown hair', weight: 0.5 }]);
+  expect(promptParser('brown hair:')).toStrictEqual([{ prompt:'brown hair', weight: 1 }]);
 });
   
